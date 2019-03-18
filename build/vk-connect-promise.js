ea6a1521c764590a3fc6e8d3fcf2123c7e64ce3d
Р“Р»РµР± Р’РѕСЂРѕРЅС†РѕРІ <g.vorontcov@corp.vk.com>
2019-03-18 18:44:34 +0300

    supports

--- build/vk-connect-promise.js
+++ build/vk-connect-promise.js
@@ -43,91 +43,126 @@ if (!window.CustomEvent) {
 
 var FUNCTION = 'function';
 var UNDEFINED = 'undefined';
-var isWeb = typeof window !== UNDEFINED && !window.AndroidBridge && !window.webkit;
+var EVENT_PREFIX = 'VKWebApp';
+var isClient = typeof window !== UNDEFINED;
+var androidBridge = isClient && window.AndroidBridge;
+var iosBridge = isClient && window.webkit && window.webkit.messageHandlers;
+var isWeb = !androidBridge && !iosBridge;
 var eventType = isWeb ? 'message' : 'VKWebAppEvent';
 var promises = {};
+var desktopEvents = [];
 var method_counter = 0;
+
+function getUrlParams(search) {
+  var hashes = search.slice(search.indexOf('?') + 1).split('&');
+  var params = {};
+  hashes.map(function (hash) {
+    var _hash$split = hash.split('='),
+        key = _hash$split[0],
+        val = _hash$split[1];
+
+    params[key] = decodeURIComponent(val);
+  });
+  return params;
+}
+
 window.addEventListener(eventType, function (event) {
   var promise = null;
-  var reponse = {};
+  var response = {};
 
   if (isWeb) {
     if (event.data && event.data.data) {
-      reponse = _extends({}, event.data);
-      promise = promises[reponse.data.request_id];
+      response = _extends({}, event.data);
+      promise = promises[response.data.request_id];
+
+      if (response.type === 'VkWebAppInitResult' && response.data['events_list']) {
+        desktopEvents = [].concat(response.data['events_list']);
+      }
     }
   } else if (event.detail && event.detail.data) {
-    reponse = _extends({}, event.detail);
-    promise = promises[reponse.data.request_id];
+    response = _extends({}, event.detail);
+    promise = promises[response.data.request_id];
   }
 
-  if (reponse.data && reponse.data.request_id) {
-    promise = promises[reponse.data.request_id];
+  if (response.data && response.data.request_id) {
+    promise = promises[response.data.request_id];
 
     if (promise) {
       if (promise.customRequestId) {
-        delete reponse.data['request_id'];
+        delete response.data['request_id'];
       }
 
-      if (reponse.data['error_type']) {
-        promise.reject(reponse);
+      if (response.data['error_type']) {
+        return promise.reject(response);
       } else {
-        promise.resolve(reponse);
+        return promise.resolve(response);
       }
     }
   }
 });
-var index = {
-  /**
-   * Sends a message to native client
-   *
-   *
-   * @param {String} handler Message type
-   * @param {Object} params Message data
-   * @returns {Promise}
-   */
-  send: function send(handler, params) {
-    if (!params) {
-      params = {};
-    }
+var index = (function () {
+  var urlParams = getUrlParams(window.location.href);
 
-    var isClient = typeof window !== UNDEFINED;
-    var androidBridge = isClient && window.AndroidBridge;
-    var iosBridge = isClient && window.webkit && window.webkit.messageHandlers;
-    var isDesktop = !androidBridge && !iosBridge;
-    var id = params['request_id'] ? params['request_id'] : "method#" + method_counter++;
-    var customRequestId = false;
-
-    if (!params.hasOwnProperty('request_id')) {
-      customRequestId = true;
-      params['request_id'] = id;
-    }
-
-    if (androidBridge && typeof androidBridge[handler] === FUNCTION) {
-      androidBridge[handler](JSON.stringify(params));
-    }
-
-    if (iosBridge && iosBridge[handler] && typeof iosBridge[handler].postMessage === FUNCTION) {
-      iosBridge[handler].postMessage(params);
-    }
-
-    if (isDesktop) {
-      parent.postMessage({
-        handler: handler,
-        params: params,
-        type: 'vk-connect'
-      }, '*');
-    }
-
-    return new Promise(function (resolve, reject) {
-      promises[id] = {
-        resolve: resolve,
-        reject: reject,
-        params: params,
-        customRequestId: customRequestId
-      };
+  if (urlParams['vk_events']) {
+    desktopEvents = urlParams['vk_events'].split(',').map(function (event) {
+      return EVENT_PREFIX + event;
     });
   }
-};
+
+  return {
+    /**
+     * Sends a message to native client
+     *
+     *
+     * @param {String} handler Message type
+     * @param {Object} params Message data
+     * @returns {Promise}
+     */
+    send: function send(handler, params) {
+      if (!params) {
+        params = {};
+      }
+
+      var id = params['request_id'] ? params['request_id'] : "method#" + method_counter++;
+      var customRequestId = false;
+
+      if (!params.hasOwnProperty('request_id')) {
+        customRequestId = true;
+        params['request_id'] = id;
+      }
+
+      if (androidBridge && typeof androidBridge[handler] === FUNCTION) {
+        androidBridge[handler](JSON.stringify(params));
+      }
+
+      if (iosBridge && iosBridge[handler] && typeof iosBridge[handler].postMessage === FUNCTION) {
+        iosBridge[handler].postMessage(params);
+      }
+
+      if (isWeb) {
+        parent.postMessage({
+          handler: handler,
+          params: params,
+          type: 'vk-connect'
+        }, '*');
+      }
+
+      return new Promise(function (resolve, reject) {
+        promises[id] = {
+          resolve: resolve,
+          reject: reject,
+          params: params,
+          customRequestId: customRequestId
+        };
+      });
+    },
+    supports: function supports(handler) {
+      if (androidBridge && typeof androidBridge[handler] === FUNCTION) return true;
+      if (iosBridge && iosBridge[handler] && typeof iosBridge[handler].postMessage === FUNCTION) return true;
+      if (~desktopEvents.indexOf(handler)) return true;
+      return false;
+    }
+  };
+})();
 
 module.exports = index;
--- src/index.js
+++ src/index.js
@@ -1,88 +1,109 @@
-import '@babel/polyfill';
-import './custom-event-polyfill';
 /* global window, parent */
 /* eslint no-restricted-globals: ["off", "parent"] */
+import '@babel/polyfill';
+import './custom-event-polyfill';
+import getUrlParams from './get-url-params';
 
 const FUNCTION = 'function';
 const UNDEFINED = 'undefined';
-const isWeb = typeof window !== UNDEFINED && !window.AndroidBridge && !window.webkit;
+const EVENT_PREFIX = 'VKWebApp';
+const isClient = typeof window !== UNDEFINED;
+const androidBridge = isClient && window.AndroidBridge;
+const iosBridge = isClient && window.webkit && window.webkit.messageHandlers;
+const isWeb = !androidBridge && !iosBridge;
 const eventType = isWeb ? 'message' : 'VKWebAppEvent';
 const promises = {};
+let desktopEvents = [];
 let method_counter = 0;
 
 window.addEventListener(eventType, (event) => {
   let promise = null;
-  let reponse = {};
+  let response = {};
   if (isWeb) {
     if (event.data && event.data.data) {
-      reponse = { ...event.data };
-      promise = promises[reponse.data.request_id];
+      response = { ...event.data };
+      promise = promises[response.data.request_id];
+      if (response.type === 'VkWebAppInitResult' && response.data['events_list']) {
+        desktopEvents = [ ...response.data['events_list'] ];
+      }
     }
   } else if (event.detail && event.detail.data) {
-    reponse = { ...event.detail };
-    promise = promises[reponse.data.request_id];
+    response = { ...event.detail };
+    promise = promises[response.data.request_id];
   }
-  if (reponse.data && reponse.data.request_id) {
-    promise = promises[reponse.data.request_id];
+
+  if (response.data && response.data.request_id) {
+    promise = promises[response.data.request_id];
     if (promise) {
       if (promise.customRequestId) {
-        delete reponse.data['request_id'];
+        delete response.data['request_id'];
       }
-      if (reponse.data['error_type']) {
-        promise.reject(reponse);
+      if (response.data['error_type']) {
+        return promise.reject(response);
       } else {
-        promise.resolve(reponse);
+        return promise.resolve(response);
       }
     }
   }
 });
 
-export default {
-  /**
-   * Sends a message to native client
-   *
-   *
-   * @param {String} handler Message type
-   * @param {Object} params Message data
-   * @returns {Promise}
-   */
-  send: (handler, params) => {
-    if (!params) {
-      params = {};
-    }
+export default (() => {
+  const urlParams = getUrlParams(window.location.href);
+  if (urlParams['vk_events']) {
+    desktopEvents = urlParams['vk_events'].split(',').map((event) => EVENT_PREFIX + event);
+  }
+  return {
+    /**
+     * Sends a message to native client
+     *
+     *
+     * @param {String} handler Message type
+     * @param {Object} params Message data
+     * @returns {Promise}
+     */
+    send: (handler, params) => {
+      if (!params) {
+        params = {};
+      }
+      const id = params['request_id'] ? params['request_id'] : `method#${method_counter++}`;
+      let customRequestId = false;
+      if (!params.hasOwnProperty('request_id')) {
+        customRequestId = true;
+        params['request_id'] = id;
+      }
 
-    var isClient = typeof window !== UNDEFINED;
-    var androidBridge = isClient && window.AndroidBridge;
-    var iosBridge = isClient && window.webkit && window.webkit.messageHandlers;
-    var isDesktop = !androidBridge && !iosBridge;
-    var id = params['request_id'] ? params['request_id'] : `method#${method_counter++}`;
-    var customRequestId = false;
-    if (!params.hasOwnProperty('request_id')) {
-      customRequestId = true;
-      params['request_id'] = id;
-    }
+      if (androidBridge && typeof androidBridge[handler] === FUNCTION) {
+        androidBridge[handler](JSON.stringify(params));
+      }
+      if (iosBridge && iosBridge[handler] && typeof iosBridge[handler].postMessage === FUNCTION) {
+        iosBridge[handler].postMessage(params);
+      }
 
-    if (androidBridge && typeof androidBridge[handler] === FUNCTION) {
-      androidBridge[handler](JSON.stringify(params));
-    }
-    if (iosBridge && iosBridge[handler] && typeof iosBridge[handler].postMessage === FUNCTION) {
-      iosBridge[handler].postMessage(params);
-    }
+      if (isWeb) {
+        parent.postMessage({
+          handler,
+          params,
+          type: 'vk-connect',
+        }, '*');
+      }
 
-    if (isDesktop) {
-      parent.postMessage({
-        handler,
-        params,
-        type: 'vk-connect',
-      }, '*');
-    }
-    return new Promise((resolve, reject) => {
-      promises[id] = {
-        resolve,
-        reject,
-        params,
-        customRequestId,
-      };
-    });
-  },
-};
+      return new Promise((resolve, reject) => {
+        promises[id] = {
+          resolve,
+          reject,
+          params,
+          customRequestId,
+        };
+      });
+    },
+    supports: (handler) => {
+      if (androidBridge && typeof androidBridge[handler] === FUNCTION) return true;
+
+      if (iosBridge && iosBridge[handler] && typeof iosBridge[handler].postMessage === FUNCTION) return true;
+
+      if (~desktopEvents.indexOf(handler)) return true;
+
+      return false;
+    },
+  };
+})();
